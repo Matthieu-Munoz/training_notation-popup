2bed849c2a6b2c3394929cf57201d90bcb8b8eaa
Matthieu-Munoz <matthieu.munoz.perso@gmail.com>
2022-04-18 22:50:39 +0200

    purge

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,12 +1,10 @@
 // == Import
-import reactLogo from './react-logo.svg';
-import './styles.css';
+import './styles.scss';
 
 // == Composant
 function App() {
   return (
     <div className="app">
-      <img src={reactLogo} alt="react logo" />
       <h1>Composant : App</h1>
     </div>
   );
