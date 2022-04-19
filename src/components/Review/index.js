25a6e898d6f98ed484d9d7818b9d406484a27b7c
Matthieu-Munoz <matthieu.munoz.perso@gmail.com>
2022-04-19 17:13:36 +0200

    Integration

--- src/components/Review/index.js
+++ src/components/Review/index.js
@@ -1,20 +1,30 @@
-// == Import
-import { useSelector } from 'react-redux';
-// Composant
-import Succes from '../Succes';
-import SubmitReview from '../SubmitReview';
 // Style
 import './styles.scss';
 
 // == Composant
 function Review() {
-  const rewiewSent = useSelector((state) => state.rewiewSent);
   return (
     <div className="review">
       <div className="review--content">
-        {rewiewSent
-          ? <Succes />
-          : <SubmitReview />}
+        <header className="review__header">
+          <div className="review__header__icon" />
+        </header>
+        <div className="review__content">
+          <h2 className="review__content__title">Qu'en avez-vous pensé ?</h2>
+          <p className="review__content__text">Vous pouvez donner votre avis sur ce que vous venez de voir. Tous
+            les commentaires comptent pour m'aider à m'améliorer !
+          </p>
+        </div>
+        <div className="review__ratings">
+          <button type="button" className="review__ratings__btn">1</button>
+          <button type="button" className="review__ratings__btn">2</button>
+          <button type="button" className="review__ratings__btn review__ratings__btn--selected">3</button>
+          <button type="button" className="review__ratings__btn">4</button>
+          <button type="button" className="review__ratings__btn">5</button>
+          <button type="button" className="review__ratings__btn review__ratings__btn--comment">C</button>
+        </div>
+
+        <button className="review__submit" type="button">Envoyer</button>
       </div>
     </div>
   );
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,59 +1,13 @@
 import {
-  SELECT_RATING,
-  TOGGLE_COMMENT,
-  INPUT_COMMENT,
-  SWITCH_SUCCES,
-  GO_BACK,
+
 } from '../actions';
 
 const initialState = {
-  rewiewSent: false,
-  rate: 0,
-  comment: {
-    isOpen: false,
-    value: '',
-  },
+
 };
 
 function reducer(state = initialState, action = {}) {
   switch (action.type) {
-    case SELECT_RATING:
-      return {
-        ...state,
-        rate: action.value,
-      };
-    case TOGGLE_COMMENT:
-      return {
-        ...state,
-        comment: {
-          ...state.comment,
-          isOpen: !state.comment.isOpen,
-        },
-      };
-    case INPUT_COMMENT:
-      return {
-        ...state,
-        comment: {
-          ...state.comment,
-          value: action.value,
-        },
-      };
-    case SWITCH_SUCCES:
-      return {
-        ...state,
-        rewiewSent: true,
-      };
-    case GO_BACK:
-      return {
-        ...state,
-        rewiewSent: false,
-        rate: 0,
-        comment: {
-          ...state.comment,
-          isOpen: false,
-          value: '',
-        },
-      };
     default:
       return state;
   }
